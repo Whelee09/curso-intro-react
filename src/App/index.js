9ebdcc5fe4c172301f9f00638f320ef8f480697a
Williangel Quevedo <williangel10@outlook.com>
2023-11-30 15:53:15 -0400

    agregue un archivo de notas, ademas aprendimos lo basico de efectos

--- src/App/index.js
+++ src/App/index.js
@@ -22,6 +22,29 @@ function App() {
     todo => !!todo.completed
   ).length;
 
+  const totalTodos = todos.length;
+
+    console.log('Log 1');
+    // React.useEffect(()=>{
+    //   console.log('Loog 2');
+    //   //parte demorada o pesada
+    //   //se ejecuta log1,log2 y log 3 cada vez qeu se renderiza
+    // });
+
+    // React.useEffect(()=>{
+    //   console.log('Loooog 2');
+      //con el array vacio como segundo parametro solo se ejecuta una vez
+    // }, []);
+
+    React.useEffect(()=>{
+      console.log('Loooog 2');
+      //ejecuta el log2 solo cuando cambia el estado de totalTodos
+    }, [totalTodos]);
+
+    console.log('Log 3');
+
+
+
   const searchedTodos = todos.filter(
     (todo) =>{
       const todoText = todo.text.toLowerCase();
@@ -29,9 +52,6 @@ function App() {
       return todoText.includes(searchValueText);
     }
   );
-
-
-  const totalTodos = todos.length;
 
   const completeTodo = (text) => {
     const newTodos = [...todos];
